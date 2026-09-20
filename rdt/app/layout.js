11715ad11f213e92bc0d5c7137d79e60744e37c2
HighTicketElites <info@highticketelites.com>
2026-09-19 22:15:48 -0400

    RDT Git engine: add rdt/app/layout.js

---
+++ rdt/app/layout.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: 'ResideTerra SEO Draft Generator',
+  robots: { index: false, follow: false },
+};
+export default function RootLayout({ children }) {
+  return <html lang="en"><body style={{margin:0,background:'#f4f6f8',color:'#111827'}}>{children}</body></html>;
+}
